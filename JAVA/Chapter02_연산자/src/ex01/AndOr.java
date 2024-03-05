9eff1ee552a8c61faf33e8f60ee8f6ea557c0f43
황금 바바나 <63435073+woo677@users.noreply.github.com>
2024-03-05 20:42:39 +0900

    Add files via upload

---
+++ JAVA/Chapter02_연산자/src/ex01/AndOr.java
@@ -0,0 +1,21 @@
+package ex01;
+
+public class AndOr {
+
+	public static void main(String[] args) {
+		
+		int val = 6;
+		
+//		AND
+		if (val>1 && val <7) {
+			System.out.println(val+" 은 1보다 크고 7보다 작군요");
+		}
+		
+//		OR
+		if (val%2==0 || val%3==0) {
+			System.out.println("2 또는 3의 배수");
+		}
+
+	}
+
+}
---
+++ JAVA/Chapter02_연산자/src/ex01/Mod_나머지.java
@@ -0,0 +1,14 @@
+package ex01;
+
+public class Mod_나머지 {
+
+	public static void main(String[] args) {
+		// %, mod 연산, 나머지 연산
+		// 나눈 몫을 제외한 나머지만 반환
+		// % 는 홀수,짝수 구분 또는 배수찾기에 자주 활용 된다.
+		
+		System.out.println("5%2="+5%2);
+
+	}
+
+}
